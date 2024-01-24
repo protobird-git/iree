7b3b6f073200072b3eb6fbee6994b6090186efc3
Byungchul Kim <byungchul@google.com>
2024-01-24 09:41:27 -0800

    Fix a bug in executing subcommand.
    1) argv in execv() must end with nullptr.
    2) Use execvp() instead of execv() for more flexibility in subcommand
       path.
    3) Set stderrthreshold for logging to 0 to log INFO and WARNING to
       stderr by default.
    4) Link absl::log_flags always.

--- tools/iree-prof-output-utils.cc
+++ tools/iree-prof-output-utils.cc
@@ -6,9 +6,13 @@
 
 #include "tools/iree-prof-output-utils.h"
 
-#include "third_party/tracy/server/TracyWorker.hpp"
+#include "third_party/abseil-cpp/absl/base/log_severity.h"
+#include "third_party/abseil-cpp/absl/flags/parse.h"
+#include "third_party/abseil-cpp/absl/log/globals.h"
+#include "third_party/abseil-cpp/absl/log/initialize.h"
 #include "third_party/abseil-cpp/absl/time/clock.h"
 #include "third_party/abseil-cpp/absl/time/time.h"
+#include "third_party/tracy/server/TracyWorker.hpp"
 
 namespace iree_prof {
 
@@ -24,6 +28,14 @@ const char* GetThreadName(const tracy::Worker& worker, uint16_t tid) {
 
 void YieldCpu() {
   absl::SleepFor(absl::Milliseconds(100));
+}
+
+std::vector<char*> InitializeLogAndParseCommandLine(int argc, char* argv[]) {
+  // Set default logging level to stderr to INFO. It can be overridden by
+  // --stderrthreshold flag.
+  absl::SetStderrThreshold(absl::LogSeverityAtLeast::kInfo);
+  absl::InitializeLog();
+  return absl::ParseCommandLine(argc, argv);
 }
 
 }  // namespace iree_prof
